4aa55908b2f9a14f6b5ad047add22ff18fce35a2
Norbert de Langen <ndelangen@me.com>
2024-03-28 22:26:07 +0100

    Add Alliance, Harkonnen, and Fremen stories

--- src/card/alliance/Alliance.stories.ts
+++ src/card/alliance/Alliance.stories.ts
@@ -33,3 +33,55 @@ export const Atreides: Story = {
     troop: 'vector/troop/atreides.svg',
   },
 };
+
+export const Harkonnen: Story = {
+  args: {
+    background: `/generated/utils/background/harkonnen.jpg`,
+    logo: 'vector/logo/harkonnen.svg',
+    name: 'Harkonnen',
+    decals: [
+      {
+        id: 'vector/decal/chaumurky.svg',
+        muted: true,
+        offset: [-40, 40],
+        outline: false,
+        scale: 1.6,
+      },
+      {
+        id: 'vector/decal/blade.svg',
+        muted: true,
+        offset: [220, -80],
+        outline: false,
+        scale: 0.8,
+      },
+    ],
+    text: 'Text goes here',
+    troop: 'vector/troop/harkonnen.svg',
+  },
+};
+
+export const Fremen: Story = {
+  args: {
+    background: `/generated/utils/background/fremen.jpg`,
+    logo: 'vector/logo/fremen.svg',
+    name: 'Fremen',
+    decals: [
+      {
+        id: 'vector/decal/shai-halud1.svg',
+        muted: true,
+        offset: [50, 40],
+        outline: false,
+        scale: 1,
+      },
+      {
+        id: 'vector/decal/herb.svg',
+        muted: true,
+        offset: [280, 0],
+        outline: false,
+        scale: 1.3,
+      },
+    ],
+    text: 'Text goes here',
+    troop: 'vector/troop/fremen.svg',
+  },
+};
