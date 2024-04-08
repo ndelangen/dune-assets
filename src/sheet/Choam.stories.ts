b3585fc4aa9f731fdf5cb68eee1ba936843d3e7c
Laurens1234 <89664514+Laurens1234@users.noreply.github.com>
2024-04-08 22:20:49 +0200

    Update Choam.stories.ts

--- src/sheet/Choam.stories.ts
+++ src/sheet/Choam.stories.ts
@@ -22,7 +22,7 @@ export const Choam: Story = {
       karama: `No interaction.`,
     },
     fate: {
-      title: 'Landsraad Contract',
+      title: 'LANDSRAAD CONTRACT',
       text: `Play at the start of any phase. This contract can copy any other contract’s ability for an additional use each turn.`,
     },
     rules: [
@@ -30,48 +30,48 @@ export const Choam: Story = {
         text: 'You are an economic powerhouse funding an army of mercenaries.',
       },
       {
-        title: 'Contracts:',
+        title: 'CONTRACTS:',
         text: `Each contract is once per turn. You may use contracts at any time unless otherwise specified. To use a contract, pay 2 spice to the spice bank.`,
         karama:
           'You cannot pay to use any contracts this phase (except fate). If this ability is karama\'ed, you can still use the contract in a next phase if possible. If any other contract is karama’ed, it’s once per turn is used. Return the spice paid when this or any contract is karama’ed.',
       },
       {
-        title: 'CHOAM Contract:',
+        title: 'CHOAM CONTRACT:',
         text: `You may trade a treachery card with another faction. The trade must be two-way (each faction giving and receiving a card).`,
         karama: 'You cannot trade.',
       },
       {
-        title: 'Emperor Contract:',
+        title: 'EMPEROR CONTRACT:',
         text: `Discard a worthless or duplicate card to gain 7 spice.`,
         karama: 'You cannot discard. Undo the discard and spice gain.',
       },
       {
-        title: 'Tleilaxu Contract:',
+        title: 'TLEILAXU CONTRACT:',
         text: `Revive 2 of your forces for free.`,
         karama: 'You cannot revive 2 forces for free.',
       },
       {
-        title: 'Atreides Contract:',
+        title: 'ATREIDES CONTRACT:',
         text: `When a stack of your forces is about to be killed by the storm or a worm, you may pay 1 spice per force to send them to the polar sink.`,
         karama: 'You cannot pay to save their forces.',
       },
       {
-        title: 'Ixian Contract:',
+        title: 'IXIAN CONTRACT:',
         text: `When a card is up for bid, before anyone bids on it, you may swap it with a worthless or duplicate card. Atreides looks. Ixians decide if they Technology before you do.`,
         karama: 'You cannot swap with the card up for bid.',
       },
       {
-        title: 'Spacing Guild Contract:',
+        title: 'SPACING GUILD CONTRACT:',
         text: `On your shipment turn, you may ship an additional time directly to a spice blow at stronghold rates.`,
         karama: 'You cannot ship an additional time.',
       },
       {
-        title: 'Bene Gesserit Contract:',
+        title: 'BENE GESSERIT CONTRACT:',
         text: `In combat (step after the Voice), you may force your opponent to answer a yes or no question about the treachery cards they have.`,
         karama: 'You cannot ask a question.',
       },
       {
-        title: 'Harkonnen Contract:',
+        title: 'HARKONNEN CONTRACT:',
         text: `When you collect spice from a spice blow or spice mine you may kill the forces needed to collect it to gain the same amount again from the spice bank.`,
         karama: 'You cannot kill forces to collect more spice.',
       },
