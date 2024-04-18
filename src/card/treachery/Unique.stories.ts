0b62af4bf9b0027464c7300aa6b1f0e198d9dc3e
Norbert de Langen <ndelangen@me.com>
2024-04-18 23:15:29 +0200

    fix

--- src/card/treachery/Unique.stories.ts
+++ src/card/treachery/Unique.stories.ts
@@ -83,7 +83,7 @@ export const BreakConditioning: Story = {
         scale: 0.6,
       },
     ],
-    text: 'You may always play this after revealing your battle plan.\nIf you\'re not called traitor, win the battle and your total battle strength is twice or more of your opponent\'s: you win as if by traitor.\nIf you\'re called traitor after playing this card you may return it to your hand.',
+    text: "You may always play this after revealing your battle plan.\nIf you're not called traitor, win the battle and your total battle strength is twice or more of your opponent's: you win as if by traitor.\nIf you're called traitor after playing this card you may return it to your hand.",
     subName: 'Special - Battle',
   },
 };
@@ -103,7 +103,6 @@ export const Amal: Story = {
         scale: 0.6,
       },
     ],
-    text: 'At the beginning of any phase, cause all players to discard half of the spice behind their shields, rounded up, to the Spice Bank.',
     text: 'At the beginning of any phase, cause all players to discard half of the spice behind their shields, rounded up, to the Spice Bank.',
     subName: 'Special - Instant',
   },
@@ -125,7 +124,6 @@ export const FamilyAtomics: Story = {
       },
     ],
     text: 'You may play this after turn 1, when the storm has been calculated, but before it is moved, if you have forces on or adjacent to the Shield Wall (with no storm inbetween).\n\nAll forces on the Shield Wall are killed. Place the Destroyed Shield Wall token on the Shield Wall. The Imperial Basin, Arrakeen and Carthag are no longer protected from the Storm.',
-    text: 'You may play this after turn 1, when the storm has been calculated, but before it is moved, if you have forces on or adjacent to the Shield Wall (with no storm inbetween).\n\nAll forces on the Shield Wall are killed. Place the Destroyed Shield Wall token on the Shield Wall. The Imperial Basin, Arrakeen and Carthag are no longer protected from the Storm.',
     subName: 'Special - Storm phase',
   },
 };
@@ -145,7 +143,7 @@ export const Hajr: Story = {
         scale: 0.8,
       },
     ],
-    text: 'Play during Movement Phase.\n\nMake an extra on-planet force movement subject to normal movement rules.\n\nThe forces you move may be a group you\'ve already moved this phase or another group.',
+    text: "Play during Movement Phase.\n\nMake an extra on-planet force movement subject to normal movement rules.\n\nThe forces you move may be a group you've already moved this phase or another group.",
     subName: 'Special - Movement',
   },
 };
@@ -166,7 +164,6 @@ export const Harvester: Story = {
       },
     ],
     text: 'Play just after a spice blow comes up. Doubles the spice blow. Place double the amount of spice in the territory.',
-    text: 'Play just after a spice blow comes up. Doubles the spice blow. Place double the amount of spice in the territory.',
     subName: 'Special - Spice blow',
   },
 };
@@ -186,7 +183,7 @@ export const Karama: Story = {
         scale: 0.9,
       },
     ],
-    text: 'Play this card to do one of the following:\n• Disable a faction advantage for the rest of the phase. May be played as a faction advantage is being used.,\n• Pay Guild rates (half price) when you or your ally ships forces onto the planet.\n• If your hand is not full and it\'s your turn to bid, purchase the treachery card now up for bid without paying spice.',
+    text: "Play this card to do one of the following:\n• Disable a faction advantage for the rest of the phase. May be played as a faction advantage is being used.,\n• Pay Guild rates (half price) when you or your ally ships forces onto the planet.\n• If your hand is not full and it's your turn to bid, purchase the treachery card now up for bid without paying spice.",
     subName: 'Special - Instant',
   },
 };
@@ -207,10 +204,9 @@ export const Thumper: Story = {
       },
     ],
     text: 'Play at beginning of Spice Blow Phase instead of revealing the next Spice Blow card. Causes a Sandworm to appear.\n\nPlay proceeds as though Shai-Hulud has been revealed.',
-    text: 'Play at beginning of Spice Blow Phase instead of revealing the next Spice Blow card. Causes a Sandworm to appear.\n\nPlay proceeds as though Shai-Hulud has been revealed.',
     subName: 'Special - Spice blow',
   },
-}; 
+};
 
 export const TleilaxuGhola: Story = {
   args: {
@@ -227,7 +223,6 @@ export const TleilaxuGhola: Story = {
         scale: 0.8,
       },
     ],
-    text: 'Play at any time.\n\nYou may immediately revive 1 of your leaders or up to 5 of your forces at no cost. You still get your normal revivals this turn.',
     text: 'Play at any time.\n\nYou may immediately revive 1 of your leaders or up to 5 of your forces at no cost. You still get your normal revivals this turn.',
     subName: 'Special - Instant',
   },
@@ -249,7 +244,6 @@ export const TruthTrance: Story = {
       },
     ],
     text: 'Play at any time.\nPublicly ask one other player a yes/no question that must be answered publicly. The game pauses until an answer is given.\nIf a player answers a question about a future action they cannot make fulfilling that action impossible. They may however take actions that lower the chance of that action being fulfilled.',
-    text: 'Play at any time.\nPublicly ask one other player a yes/no question that must be answered publicly. The game pauses until an answer is given.\nIf a player answers a question about a future action they cannot make fulfilling that action impossible. They may however take actions that lower the chance of that action being fulfilled.',
     subName: 'Special - Instant',
   },
 };
@@ -269,7 +263,6 @@ export const WeatherControl: Story = {
         scale: 0.9,
       },
     ],
-    text: 'After the first game turn, play during the Storm Phase after seeing how far the storm moves.You control the storm this turn instead. Pick a number from 0 to 10 and move the storm that far.',
     text: 'After the first game turn, play during the Storm Phase after seeing how far the storm moves.You control the storm this turn instead. Pick a number from 0 to 10 and move the storm that far.',
     subName: 'Special - Instant',
   },
@@ -351,7 +344,6 @@ export const JuiceOfSaphoAlt: Story = {
       },
     ],
     text: 'Choose one:\nPlay this card to be considered the aggressor in a battle, or\nGo first for any phase or action that requires turn order, or\nGo last for any phase or action that requires turn order (even superseding Guild).\nPlay the card in the applicable phase when you wish to intervene. Discard after use.',
-    text: 'Choose one:\nPlay this card to be considered the aggressor in a battle, or\nGo first for any phase or action that requires turn order, or\nGo last for any phase or action that requires turn order (even superseding Guild).\nPlay the card in the applicable phase when you wish to intervene. Discard after use.',
     subName: 'Special - Instant',
   },
 };
@@ -372,7 +364,6 @@ export const ResidualPoison: Story = {
       },
     ],
     text: 'Play on your opponent before choosing leaders in a battle. That faction will lose one of their available leaders at random to the tanks. No spice is collected for it.\nDiscard after use.',
-    text: 'Play on your opponent before choosing leaders in a battle. That faction will lose one of their available leaders at random to the tanks. No spice is collected for it.\nDiscard after use.',
     subName: 'Special - Instant',
   },
 };
@@ -392,7 +383,6 @@ export const Ornithopter: Story = {
         scale: 0.8,
       },
     ],
-    text: 'As part of your movement, you may move one group of your forces up to 3 territories, or move 2 different groups of your forces up to your normal movement. Discard after use.',
     text: 'As part of your movement, you may move one group of your forces up to 3 territories, or move 2 different groups of your forces up to your normal movement. Discard after use.',
     subName: 'Special - Instant',
   },
@@ -434,7 +424,6 @@ export const SemutaDrug: Story = {
       },
     ],
     text: 'Take a Treachery Card immediately after another player discards it, and add it to your hand.\nIf multiple cards are discarded at the same time, you choose which card to take. Discard after use.',
-    text: 'Take a Treachery Card immediately after another player discards it, and add it to your hand.\nIf multiple cards are discarded at the same time, you choose which card to take. Discard after use.',
     subName: 'Special - Instant',
   },
 };
@@ -455,7 +444,6 @@ export const NullentropyBox: Story = {
       },
     ],
     text: 'At any time, pay 2 spice to the Spice Bank, then take the Treachery Card discard pile and secretly search it for any card (other than a Nullentropy Box) to add to your hand. Shuffle the discard pile afterward and then return it face up, discarding this card on top.',
-    text: 'At any time, pay 2 spice to the Spice Bank, then take the Treachery Card discard pile and secretly search it for any card (other than a Nullentropy Box) to add to your hand. Shuffle the discard pile afterward and then return it face up, discarding this card on top.',
     subName: 'Special - Instant',
   },
 };
@@ -466,7 +454,6 @@ export const Distrance: Story = {
     icon: [`/generated/utils/background/striped-special.jpg`, 'vector/icon/hand-alt.svg'],
     iconOffset: [0, 2],
     name: 'Distrans',
-    name: 'Distrans',
     decals: [
       {
         id: 'vector/decal/distrance.svg',
@@ -476,7 +463,7 @@ export const Distrance: Story = {
         scale: 1,
       },
     ],
-    text: 'Give another player a Treachery Card from your hand (if that player\'s hand is not full). Play at any time (other than during a bid). Discard after use.',
+    text: "Give another player a Treachery Card from your hand (if that player's hand is not full). Play at any time (other than during a bid). Discard after use.",
     subName: 'Special - Instant',
   },
 };
