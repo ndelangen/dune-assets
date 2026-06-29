e56d88c0cb00dbe94654528e0772edd6f28ba1a1
Jac3kC <jacek@czyrnek.net>
2026-06-30 00:04:19 +0200

    Add missing card description to Portable Snooper Treachery Card

--- src/card/treachery/Defense.stories.ts
+++ src/card/treachery/Defense.stories.ts
@@ -65,7 +65,7 @@ export const PortableSnooper = meta.story({
         scale: 0.6,
       },
     ],
-    text: 'Text goes here',
+    text: 'Protects your leader from a poison weapon. You may play this after revealing your Battle Plan if you did not play a defense (unless prevented by the Bene Gesserit Voice). You may not play this if you played a weapon and a worthless card in your Battle Plan. Discard after use.',
     subName: 'Defense - Poison - Special',
   },
 });
